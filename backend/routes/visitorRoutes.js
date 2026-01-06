c64693aeba324a364362053effd0aabe1bc05cdf
Sujan.k <147074584+bdu1721mt2334@users.noreply.github.com>
2026-01-06 20:33:27 +0530

    Update visitorRoutes.js

--- backend/routes/visitorRoutes.js
+++ backend/routes/visitorRoutes.js
@@ -7,13 +7,10 @@ const {
   checkOutVisitor
 } = require("../controllers/visitorController");
 
-// Add visitor
 router.post("/", addVisitor);
 
-// Get all visitors
 router.get("/", getVisitors);
 
-// Checkout visitor
 router.put("/checkout/:id", checkOutVisitor);
 
 module.exports = router;
